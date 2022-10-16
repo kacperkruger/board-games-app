f52d8292317e9894b558c0ea5e8a6fa133bad78a
Kacper Kruger <kacper.kruger1@gmail.com>
2022-10-16 15:50:40 +0200

    publushers route post

--- BoardGamesApi/routes/publishers.js
+++ BoardGamesApi/routes/publishers.js
@@ -8,3 +8,25 @@ router.get('/', async (req, res) => {
     const publishers = await Publisher.find({});
     res.send(publishers);
 });
+
+router.post('/', async (req, res) => {
+    const publisherToCreate = req.body;
+    try {
+        const publisher = await Publisher.create({
+            'name': publisherToCreate.name,
+            'description': publisherToCreate.description,
+            'creation_year': publisherToCreate.creation_year,
+            'image_url': publisherToCreate.image_url,
+            'city': publisherToCreate.city,
+            'street_name': publisherToCreate.street_name,
+            'street_number': publisherToCreate.street_number,
+            'apartament_number': publisherToCreate.apartament_number,
+            'official_link': publisherToCreate.official_link
+        });
+        res.send(publisher);
+    } catch(err) {
+        console.log(err.message);
+        res.status(500).send(err.message);
+    }
+
+});
