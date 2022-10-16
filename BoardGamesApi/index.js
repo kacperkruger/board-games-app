f2563b0644587a0f796803fe7093a2d3d8f7db55
Kacper Kruger <kacper.kruger1@gmail.com>
2022-10-16 14:40:21 +0200

    db connection data

--- BoardGamesApi/index.js
+++ BoardGamesApi/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const dbConnData = {
+    host: process.env.MONGO_HOST || '127.0.0.1',
+    port: process.env.MONGO_PORT || 27017,
+    database: process.env.MONGO_DATABASE || 'BoardGamesApi'
+};
+
