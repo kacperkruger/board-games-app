629d305550e97fed914ce38786f38791a608608f
Kacper Kruger <kacper.kruger1@gmail.com>
2022-10-17 12:48:26 +0200

    redux store and middleware

--- BoardGamesApp/src/index.js
+++ BoardGamesApp/src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger'
+import thunk from 'redux-thunk';
+import gamesReducer from './ducks/games/index';
+import publisherReducer from './ducks/publishers/index';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const store = createStore(
+    combineReducers({
+        games: gamesReducer,
+        publishers: publisherReducer,
+    }), applyMiddleware(thunk, logger)
+);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
